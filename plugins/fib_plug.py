704a4f1a4bd80c882d1df92fe2c5bb19ce4fe568
sudo-191 <aviksk000@gmail.com>
2017-04-09 04:50:34 -0700

    Update fib_plug.py

--- plugins/fib_plug.py
+++ plugins/fib_plug.py
@@ -15,13 +15,13 @@ def fib_func():
             textbox.insert('end',"invalid fib input",'red')
 
 
-#without main and privilage function a plugin will be invalid
+#without main and privilege function a plugin will be invalid
 def main(textBox_widget, entry_widget):
     global textbox,entry
     textbox=textBox_widget
     entry=entry_widget
     entry.bind("<F1>",lambda x: fib_func())
 
-#privilage list elements sequence== function arguments sequence
-def privilage():
+#privilege list elements sequence== function arguments sequence
+def privilege():
     return("textBox",'entry')
