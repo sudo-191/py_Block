c16e87c711c636d640b0ca3f80b47d26bb65930f
sudo-191 <aviksk000@gmail.com>
2017-04-09 04:50:13 -0700

    Update clear.py

--- plugins/clear.py
+++ plugins/clear.py
@@ -3,5 +3,5 @@ def main(window,textBox):
     button=tk.Button(window,text="clear")
     button.config(command=lambda: textBox.delete(1.0,'end'))
     button.grid(row=1,column=0,sticky="n")
-def privilage():
+def privilege():
     return ["window","textBox"]
